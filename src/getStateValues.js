cc80ab7d3145c91c95dba393825568461523e34a
hung.nguyen <hung.nguyen@dfo.global>
2019-07-01 06:22:15 +0700

    update documentation

--- src/getStateValues.js
+++ src/getStateValues.js
@@ -2,10 +2,15 @@ import { memoize } from "./utils";
 
 export default function getStateValues(states, valueOnly) {
   return states.map(x => {
-    if (x.type === "action") return x.value;
     const [state, mapper, ...mapperArgs] = Array.isArray(x) ? x : [x];
     state.init();
-    const result = valueOnly ? state() : state.async ? state : state();
+    const result = valueOnly
+      ? x.type === "action"
+        ? x.value
+        : state()
+      : state.async
+      ? state
+      : state();
     if (mapper && !mapper.__memoizedMapper) {
       mapper.__memoizedMapper = memoize(mapper);
     }
